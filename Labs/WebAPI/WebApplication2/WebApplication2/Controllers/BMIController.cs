ce2f9db4e6532b490cc397da35e78f328c52cd90
unknown <chlin97701021@gmail.com>
2019-10-18 09:27:24 +0800

    WebAPI practice

---
+++ Labs/WebAPI/WebApplication2/WebApplication2/Controllers/BMIController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication2.Controllers
+{
+    public class BMIResult
+    {
+        public float BMI { get; set; }
+    }
+
+    public class BMIParam
+    {
+        public float height { get; set; }
+        public float weight { get; set; }
+    }
+
+    public class BMIController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult Post(BMIParam para)
+        {
+            para.height = para.height / 100;
+            var bmi = para.weight / (para.height * para.height);
+            var ret = new BMIResult() { BMI = bmi };
+            return Ok(ret);
+        }
+    }    
+}
